46bcb7dff7f4e44c0b39791bbf745a22b735ca32
drop-stones <shizukuishi.s.aa@m.titech.ac.jp>
2021-01-07 23:37:38 -0800

    day08c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -23,8 +23,10 @@
 #include "queue.hpp"
 #include "segment.hpp"
 #include "paging.hpp"
+#include "memory_manager.hpp"
 
 #define WALLPAPER_
+
 #ifdef WALLPAPER_
 #include "wallpaper.hpp"
 char wallpaper_buf[sizeof(WallPaper)];
@@ -52,6 +54,9 @@ int printk(const char* format, ...) {
   console->PutString(s);
   return result;
 }
+
+char memory_manager_buf[sizeof(BitmapMemoryManager)];
+BitmapMemoryManager* memory_manager;
 
 char mouse_cursor_buf[sizeof(MouseCursor)];
 MouseCursor* mouse_cursor;
@@ -166,21 +171,35 @@ extern "C" void KernelMainNewStack(
   
   SetupIdentityPageTable();
 
-  printk("memory_map: %p\n", &memory_map);
+  ::memory_manager = new(memory_manager_buf) BitmapMemoryManager;
+
   const auto memory_map_base = reinterpret_cast<uintptr_t>(memory_map.buffer);
+  uintptr_t available_end = 0;
   for (uintptr_t iter = memory_map_base;
        iter < memory_map_base + memory_map.map_size;
        iter += memory_map.descriptor_size) {
     auto desc = reinterpret_cast<MemoryDescriptor*>(iter);
+    if (available_end < desc->physical_start) {
+      // find empty space!!
+      memory_manager->MarkAllocated(
+        FrameID{available_end / kBytesPerFrame},
+        (desc->physical_start - available_end) / kBytesPerFrame
+      );
+    }
+
+    const auto physical_end = 
+      desc->physical_start + desc->number_of_pages * kUEFIPageSize;
     if (IsAvailable(static_cast<MemoryType>(desc->type))) {
-      printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
-             desc->type,
-             desc->physical_start,
-             desc->physical_start + desc->number_of_pages * 4096 - 1,
-             desc->number_of_pages,
-             desc->attribute);
+      available_end = physical_end;
+    } else {
+      // in use
+      memory_manager->MarkAllocated(
+        FrameID{desc->physical_start / kBytesPerFrame},
+        desc->number_of_pages * kUEFIPageSize / kBytesPerFrame
+      );
     }
   }
+  memory_manager->SetMemoryRange(FrameID{1}, FrameID{available_end / kBytesPerFrame});
   
   mouse_cursor = new(mouse_cursor_buf) MouseCursor(
     pixel_writer, kDesktopBGColor, {300, 200}
